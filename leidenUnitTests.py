eb7cae1f9edb68b43ba2212bec089ae667705e0a
mshiffman <shiffmanm@carleton.edu>
2025-02-07 12:19:22 -0600

    success & some unit tests

---
+++ leidenUnitTests.py
@@ -0,0 +1,28 @@
+import pytest
+import networkx as nx
+from leiden import Leiden
+
+# def testModAgainstNx():
+#     nxG = nx.Graph()
+#     nxG.add_edges_from([("a", "b"), ("b", "c"), ("d", "e"), ("e", "f")])  
+#     communities = [["a", "b", "c"], ["d","e", "f"]]
+#     classG = Leiden(nxG)
+#     mod = classG.modularity("a", communities[0], nxG)
+    
+#     assert mod == nx.community.modularity(nxG, communities)
+
+def testModNoNode():
+    nxG = nx.Graph()
+    nxG.add_edges_from([("a", "b"), ("b", "c"), ("d", "e"), ("e", "f")])  
+    communities = [["a", "b", "c"], ["d","e", "f"]]
+    classG = Leiden(nxG)
+    
+    with pytest.raises(ValueError):
+        classG.modularity("g", communities[0], nxG) 
+
+def testModEmpty():
+    nxG = nx.Graph()
+    nxG.add_edges_from([("a", "b"), ("b", "c"), ("d", "e"), ("e", "f")])  
+    communities = [["a", "b", "c"], ["d","e", "f"]]
+    classG = Leiden(nxG)
+    assert classG.modularity("a", communities[0], nxG) == 0 
