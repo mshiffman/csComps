f08bb1ec9385cbc74a6c081853cac2382a0f6fea
Lab User <labuser@libr451-07r.itslabs.carleton.edu>
2025-02-18 21:45:05 -0600

    ready to run louvain

--- makegraph_tracts.py
+++ makegraph_tracts.py
@@ -3,6 +3,7 @@ import networkx as nx
 import louvain_fixed
 import random
 import csv
+import pickle
 
 #must be in this order i think
 # import matplotlib
@@ -47,8 +48,6 @@ for tract in centroid_totals:
     centroidDictionary[tract] = (centroid_totals[tract][0]/centroid_totals[tract][2], 
                                  centroid_totals[tract][1]/centroid_totals[tract][2])
     graph1.add_node(tract, children = [], pos=(centroidDictionary[tract][0], centroidDictionary[tract][1]), color=None)
-#for key in centroidDictionary.keys():
-    #print(float(key), float(centroidDictionary[key][0]), float(centroidDictionary[key][1]))
 
 for i in range(1, len(edgeData)):
     start_geocode = edgeData.iloc[i,0]
@@ -72,14 +71,10 @@ for key in weightsDictionary:
     graph1.add_edge(source, dest, weight=weightsDictionary[key])
     edgeCount += 1
 
-def randomColors(n):
-    return [(random.random(), random.random(), random.random()) for _ in range(n)]
+with open('graph.pkl', 'wb') as f:
+    pickle.dump(graph1, f)
 
-graph = nx.erdos_renyi_graph(30, 0.4, seed=25)
-for u, v in graph.edges():
-    graph[u][v]['weight'] = random.uniform(0.1, 1.0)
-
-
+#run the louvain algorithm on the tract graph
 l = louvain_fixed.Louvain(graph1)
 l.run()
 
@@ -88,6 +83,17 @@ with open('louvain_data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
+
+with open('coordinate_data.csv', 'w', newline = '') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Tract", "Latitude", "Longitude"])
+    for tract in centroidDictionary.keys():
+        row = [tract, centroidDictionary[tract][0], centroidDictionary[tract][1]]
+        writer.writerow(row)
+
+''' this is a bunch of business we don't need right now
+def randomColors(n):
+    return [(random.random(), random.random(), random.random()) for _ in range(n)]
 
 #assigning the colors
 colors = randomColors(len(l.nestedCommunities))
@@ -104,7 +110,7 @@ plt.figure(figsize=(6, 6))
 nx.draw(graph1, node_color=node_colors)
 #plt.show()
 plt.savefig("/home/gordong/Desktop/louvain_1.3.svg")
-
+'''
 end_time = datetime.datetime.now()
 execution_time = end_time - start_time
 print(f"Execution time in seconds: {execution_time.total_seconds():.4f} seconds")
