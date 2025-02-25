2c94f30018a7ce3c62e0bfd99d9e5724509b7128
mshiffman <shiffmanm@carleton.edu>
2025-02-24 19:16:57 -0600

    endings

--- testGN.py
+++ testGN.py
@@ -7,12 +7,13 @@ with open("graph.pkl", 'rb') as f:
     G = pickle.load(f)
 
 print(G.number_of_nodes())
+print(G.number_of_edges())
 
 with open("girvanStats.pkl", 'rb') as f:
     girvanStats = pickle.load(f)
 
 
-print(girvanStats)
+# print(girvanStats)
 # for i in range(len(girvanStats["results"])):
 #     for j in range(len(girvanStats["results"][i])):
 #         print(j)
