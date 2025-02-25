5e1171146623482ffb4e1b490e177247f3f49ae1
shiffmanm <shiffmanm@carleton.edu>
2025-02-24 18:02:08 -0600

    temp

--- runGN.py
+++ runGN.py
@@ -3,25 +3,54 @@ from girvanNewman import GirvanNewman
 from testSuccess import *
 import time
 
-girvanStats = {"runtime":[], "modularity":[], "avgConductance":[], "avgDensity":[], "constantPotts":[], "results":[]}
-with open("graphGN.pkl", 'rb') as f:
-    G = pickle.load(f)
-
-girvanGraph = GirvanNewman(G)
-girvanStart = time.time()
-girvanResult = girvanGraph.girvanNewmanAlgo(True)
-girvanEnd = time.time()
-girvanRuntime = girvanEnd - girvanStart
-girvanStats["runtime"].append(girvanRuntime)
-girvanStats["results"].append(girvanResult)
+girvanStats1 = {"runtime":[], "results":[]}
+girvanStats2 = {"runtime":[], "results":[]}
+girvanStats3 = {"runtime":[], "results":[]}
+girvanStats4 = {"runtime":[], "results":[]}
 
 with open("graphGN.pkl", 'rb') as f:
     G = pickle.load(f)
-    
-girvanStats["modularity"].append(modularity(G, girvanStats["results"][0]))
-girvanStats["avgConductance"].append(graphAvgConductance(G,girvanStats["results"][0]))
-girvanStats["avgDensity"].append(graphAvgDensity(G,girvanStats["results"][0]))
-girvanStats["constantPotts"].append(constantPotts(G,girvanStats["results"][0], 1))
 
-with open('girvanStats.pkl', 'wb') as f:
+girvan1Graph = GirvanNewman(G)
+girvan1Start = time.time()
+girvan1Result = girvan1Graph.girvanNewmanAlgo(True, "modularity")
+girvan1End = time.time()
+girvan1Runtime = girvan1End - girvan1Start
+girvanStats1["runtime"].append(girvan1Runtime)
+girvanStats1["results"].append(girvan1Result)
+
+with open("graphGN.pkl", 'rb') as f:
+    G = pickle.load(f)
+girvan2Graph = GirvanNewman(G)
+girvan2Start = time.time()
+girvan2Result = girvan2Graph.girvanNewmanAlgo(True, "constantPotts")
+girvan2End = time.time()
+girvanRuntime = girvan2End - girvan2Start
+girvanStats2["runtime"].append(girvanRuntime)
+girvanStats2["results"].append(girvan2Result)
+
+
+with open("graphGN.pkl", 'rb') as f:
+    G = pickle.load(f)
+girvan2Graph = GirvanNewman(G)
+girvan2Start = time.time()
+girvan2Result = girvan2Graph.girvanNewmanAlgo(True, "constantPotts")
+girvan2End = time.time()
+girvanRuntime = girvan2End - girvan2Start
+girvanStats2["runtime"].append(girvanRuntime)
+girvanStats2["results"].append(girvan2Result)
+
+
+
+
+
+
+with open('girvanStats2.pkl', 'wb') as f:
     pickle.dump(girvanStats, f)
+
+print(girvanStats)
+
+# girvanStats["modularity"].append(modularity(G, girvanStats["results"][0]))
+# girvanStats["avgConductance"].append(graphAvgConductance(G,girvanStats["results"][0]))
+# girvanStats["avgDensity"].append(graphAvgDensity(G,girvanStats["results"][0]))
+# girvanStats["constantPotts"].append(constantPotts(G,girvanStats["results"][0], 1))
---
+++ testGN.py
@@ -0,0 +1,21 @@
+import pickle
+import networkx as nx
+
+with open("graphGN.pkl", 'rb') as f:
+    G = pickle.load(f)
+with open("graph.pkl", 'rb') as f:
+    G = pickle.load(f)
+
+print(G.number_of_nodes())
+
+with open("girvanStats.pkl", 'rb') as f:
+    girvanStats = pickle.load(f)
+
+
+print(girvanStats)
+# for i in range(len(girvanStats["results"])):
+#     for j in range(len(girvanStats["results"][i])):
+#         print(j)
+#         print(len(girvanStats["results"][i][j]))
+#         print(girvanStats["results"][i][j])
+#         print("\n")
