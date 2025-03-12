a0e6868f30ba73b71603113733315addb88deb5e
grahamgordon399 <gordong@carleton.edu>
2025-03-11 21:47:41 -0500

    last louvain

--- run_louvain.py
+++ run_louvain.py
@@ -11,10 +11,62 @@ with open(filePath, 'rb') as f:
 print("graph loaded")
 
 start_time = datetime.datetime.now()
+l = louvain.Louvain(graph,resolution=1)
+l.run()
+
+with open('louvainData_0/louvain_communities_1.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for comm in l.nestedCommunities:
+        writer.writerow(comm)
+
+end_time = datetime.datetime.now()
+execution_time = end_time - start_time
+print(f"Execution time in seconds: {execution_time.total_seconds():.4f} seconds")
+
+start_time = datetime.datetime.now()
+l = louvain.Louvain(graph,resolution=1.1)
+l.run()
+
+with open('louvainData_0/louvain_communities_1_1.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for comm in l.nestedCommunities:
+        writer.writerow(comm)
+
+end_time = datetime.datetime.now()
+execution_time = end_time - start_time
+print(f"Execution time in seconds: {execution_time.total_seconds():.4f} seconds")
+
+start_time = datetime.datetime.now()
+l = louvain.Louvain(graph,resolution=1.2)
+l.run()
+
+with open('louvainData_0/louvain_communities_1_2.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for comm in l.nestedCommunities:
+        writer.writerow(comm)
+
+end_time = datetime.datetime.now()
+execution_time = end_time - start_time
+print(f"Execution time in seconds: {execution_time.total_seconds():.4f} seconds")
+
+start_time = datetime.datetime.now()
+l = louvain.Louvain(graph,resolution=1.3)
+l.run()
+
+with open('louvainData_0/louvain_communities_1_3.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    for comm in l.nestedCommunities:
+        writer.writerow(comm)
+
+end_time = datetime.datetime.now()
+execution_time = end_time - start_time
+print(f"Execution time in seconds: {execution_time.total_seconds():.4f} seconds")
+
+start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=1.4)
 l.run()
 
-with open('louvain_communities_1_4.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_1_4.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
@@ -27,7 +79,7 @@ start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=1.5)
 l.run()
 
-with open('louvain_communities_1_5.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_1_5.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
@@ -40,7 +92,7 @@ start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=1.6)
 l.run()
 
-with open('louvain_communities_1_6.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_1_6.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
@@ -53,7 +105,7 @@ start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=1.7)
 l.run()
 
-with open('louvain_communities_1_7.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_1_7.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
@@ -66,7 +118,7 @@ start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=1.8)
 l.run()
 
-with open('louvain_communities_1_8.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_1_8.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
@@ -79,7 +131,7 @@ start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=1.9)
 l.run()
 
-with open('louvain_communities_1_9.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_1_9.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
@@ -92,7 +144,7 @@ start_time = datetime.datetime.now()
 l = louvain.Louvain(graph,resolution=2)
 l.run()
 
-with open('louvain_communities_2.csv', 'w', newline='') as file:
+with open('louvainData_0/louvain_communities_2.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     for comm in l.nestedCommunities:
         writer.writerow(comm)
